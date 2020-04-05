715327d2e3616fe670b1cf095e9b275d8260f7cd
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-05 16:40:54 -0400

    testing netlify continuos deployment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function App() {
   return (
-      <h1>Hello World!</h1>
+      <div>
+        <h1>Hello World!</h1>
+        <h2>Testing Netlify continuos deployment</h2>
+      </div>
   );
 }
 
