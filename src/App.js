741918f859f14120b66dfb8b07dca39ebbe7bd33
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-05 19:49:20 -0400

    added pages directory with home about and projects components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Nav from './components/Nav'
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
-      <div>
+      <Router>
         <Nav />
         <h1>Hello World!</h1>
         <h2>Testing Netlify continuos deployment</h2>
-      </div>
+      </Router>
   );
 }
 
