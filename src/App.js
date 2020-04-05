47f23b29b35e3c47712e1d224d90e3187bb6e562
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-05 19:39:54 -0400

    added nav component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React from 'react'
+import Nav from './components/Nav'
 
 function App() {
   return (
       <div>
+        <Nav />
         <h1>Hello World!</h1>
         <h2>Testing Netlify continuos deployment</h2>
       </div>
