b103d7e2705401523a8c58e9bc1ecbb71a41e09c
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-06 09:21:35 -0400

    managed to insert animation scripts from home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState ,useEffect, useRef } from 'react';
+import styles from './Home.module.css'
 
 const Home = () => {
-    return (
-        <h1>Hi there, my name is Elvis Hernandez and I'm a Full-Stack Web Developer.
+    // const homeRef = useRef(null)
 
-        </h1>
+    useEffect(() => {
+        // if(!homeRef.current) return
+
+        const appScript = document.createElement('script')
+        const particlesScript = document.createElement('script')
+        appScript.src = './particles.js'
+        particlesScript.src = './app.js'
+        appScript.async = true
+        particlesScript.async = true
+
+        document.body.appendChild(appScript)
+        document.body.appendChild(particlesScript)
+
+        return () => {
+            document.body.removeChild(appScript)
+            document.body.removeChild(particlesScript)
+        }
+    },[])
+
+    return (
+        <>
+            {/* <div id='home'> */}
+                <h1>Hi there, my name is Elvis Hernandez and I'm a Full-Stack Web Developer.</h1> 
+                {/* <section className={styles.home} id="particles-js" />         */}
+            {/* </div> */}
+            {/* {homeRef.current = document.getElementById('home')}
+            {console.log(document.getElementById('home'))} */}
+        </>
     );
 }
  
