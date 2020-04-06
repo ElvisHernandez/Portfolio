0eac717bdb6be07fa6ada5e12eb42b04468f49dd
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-06 11:16:39 -0400

    adding scripts and animation section with imported styles from home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,37 +4,25 @@ import styles from './Home.module.css'
 const Home = () => {
 
     useEffect(() => {
-        let animation = document.getElementById('particles-js')
-        if(!animation) {
-            animation = document.createElement('section')
-            animation.id = 'particles-js'
-            document.body.appendChild(animation)
-        }
         console.log('this is my console.log',document.getElementById('particles-js'))
         const appScript = document.createElement('script')
         const particlesScript = document.createElement('script')
         appScript.src = './particles.js'
         particlesScript.src = './app.js'
 
-        const animationContainer = document.getElementById('animation-container')
-        animationContainer.appendChild(appScript)
-        animationContainer.appendChild(particlesScript)
+        document.body.appendChild(appScript)
+        document.body.appendChild(particlesScript)
 
         return () => {
-            // const animation = document.getElementById('particles-js')
-
-            document.body.removeChild(animation)
+            document.body.removeChild(appScript)
+            document.body.removeChild(particlesScript)
         }
     },[])
 
     return (
-        <>
-            {/* <div id='home'> */}
-                <h1>Hi there, my name is Elvis Hernandez and I'm a Full-Stack Web Developer.</h1> 
-                {/* <section className={styles.home} id="particles-js" />         */}
-            {/* </div> */}
-            {/* {homeRef.current = document.getElementById('home')}
-            {console.log(document.getElementById('home'))} */}
+        <>                
+            <h1>Hi there, my name is Elvis Hernandez and I'm a Full-Stack Web Developer.</h1> 
+            <section id='particles-js' className={styles.particlesJs}></section>
         </>
     );
 }
