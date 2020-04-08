e42ae6f28dab04c5ee1cd62e3f927c194f824201
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-08 02:52:01 -0400

    added navbar style and removed bullets from unordered list

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from './Nav.module.css'
 
 const Nav = () => {
     return(
         <nav>
-            <ul>
+            <ul className={styles.navbar}>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/about'>About</Link></li>
                 <li><Link to='/projects'>Projects</Link></li>
