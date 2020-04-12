d7ab3d342a13f6cda5ee707643d0d0fbb576913e
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-11 23:37:26 -0400

    added colored skills divs

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -4,7 +4,7 @@ import styles from './Skills.module.css'
 const Skills = () => {
     return (
         <div className={styles.body}>
-            <h1 className={styles.heading}>Skills</h1>
+            {/* <h1 className={styles.heading}>Skills</h1> */}
             <div className={styles.skillsIntro}>Blah</div>
             <div className={styles.skills}></div>
         </div>
