838b668f52488e79a9bd68f03a4221abb89cdc41
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-12 19:44:27 -0400

    added constants directory with skills constants

---
+++ src/CONSTANTS/SkillsConstants.js
@@ -0,0 +1,7 @@
+
+
+export const frontEndDescription = `My favorite part about the 
+front-end is being able to start with a blank canvas and 
+watching it evolve into the result of your imagination.`
+
+export const frontEndSkills = ['HTML5','CSS3','React','JavaScript','Bootstrap4','Material UI']
--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -2,12 +2,8 @@ import React from 'react';
 import styles from './Skills.module.css'
 import { nodePNG, programmingPNG, javascriptPNG, elvis, reactPNG } from '../../assets'
 import { SkillsCard } from '../../components'
+import * as SkillsConstants from '../../CONSTANTS/SkillsConstants'
 
-const frontEndDescription = `My favorite part about the 
-front-end is being able to start with a blank canvas and 
-watching it evolve into the result of your imagination.`
-
-const frontEndSkills = ['HTML5','CSS3','React','JavaScript','Bootstrap4','Material UI']
 
 const Skills = () => {
     return (
@@ -17,8 +13,8 @@ const Skills = () => {
             <h1 className={styles.heading}>Skills</h1>
             <SkillsCard imgURL={reactPNG} 
                 title="Front-End Skills" 
-                description={frontEndDescription}
-                frontEndSkills={frontEndSkills} 
+                description={SkillsConstants.frontEndDescription}
+                frontEndSkills={SkillsConstants.frontEndSkills} 
             />
             <div className={styles.skillsIntro}>Blah</div>
             <div className={styles.skills}></div>
