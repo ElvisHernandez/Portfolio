a28c7ceab017b6bff69e5e88f9c05935ce55969d
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-12 18:00:35 -0400

    added material ui card and react logo to add for front end card

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './Skills.module.css'
 import { nodePNG, programmingPNG, javascriptPNG, elvis } from '../../assets'
+import { MediaCard } from '../../components'
 
 const Skills = () => {
     return (
         <div className={styles.body}>
             {/* <img style={{borderRadius:'50%', height:'auto', width: 'auto'}} src={elvis} alt=""/> */}
-            <div className={styles.image}></div>
-            {/* <h1 className={styles.heading}>Skills</h1> */}
+            {/* <div className={styles.image}></div> */} 
+            <h1 className={styles.heading}>Skills</h1>
+            <MediaCard />
             <div className={styles.skillsIntro}>Blah</div>
             <div className={styles.skills}></div>
         </div>
