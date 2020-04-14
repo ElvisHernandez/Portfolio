fb95ae666ead243fb9c10fe453d22dcc644dc939
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-14 03:24:56 -0400

    managed to get the img tags in the right position over the green projects section

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Projects.module.css'
+import { airAware, softSearch } from '../../assets'
 
 const Projects = () => {
     return (
@@ -7,8 +8,10 @@ const Projects = () => {
             <div className={styles.projectsIntro}><h1>Projects</h1></div>
             {/* <div className={styles.projects}></div> */}
             <div className={styles.projects}>
-                <span className={styles.project1}>Project 1</span>
-                <span className={styles.project2}>Project 2</span>
+                {/* <span className={`${styles.project} ${styles.project1}`}></span>
+                <span className={`${styles.project} ${styles.project2}`}></span> */}
+                <img className={styles.project} src={softSearch} alt="#" />
+                <img className={styles.project} src={airAware} alt="#" />
             </div>
         </div>
     );
