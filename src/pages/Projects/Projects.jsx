7f555b79428f75eb0618fcb54d97e08f93fdebda
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-14 17:43:49 -0400

    removed use of data state and reserved it as a store of student data for later use if necessary

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import styles from './Projects.module.css'
 import { airAware, softSearch } from '../../assets'
+import { Button } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
+
+const GlobalCSS = withStyles({
+    "@global": {
+        
+    }
+})
 
 const Projects = () => {
     return (
         <div className={styles.projectsContainer}>
-            <div className={styles.projectsIntro}><h1>Projects</h1></div>
-            {/* <div className={styles.projects}></div> */}
+            <div className={styles.projectsIntro}><h1>Projects</h1> <Button style={{paddingLeft: '20rem'}}/></div>
             <div className={styles.projects}>
                 <span className={`${styles.project} ${styles.project1}`}>
-                    <button className={styles.projectButton}>View Project</button>
-                    <button className={styles.codeButton}>View Code</button>
+                    {/* <button className={styles.projectButton}>View Project</button> */}
+                    {/* <button className={styles.codeButton}>View Code</button> */}
+                    <Button className={styles.projectButton}>View Project</Button>
+                    <Button className={styles.codeButton}>View Code</Button>
+
                 </span>
                 <span className={`${styles.project} ${styles.project2}`}>
-                    <button className={styles.projectButton}>View Project</button>
-                    <button className={styles.codeButton}>View Code</button>
+                    {/* <button className={styles.projectButton}>View Project</button>
+                    <button className={styles.codeButton}>View Code</button> */}
+                    <Button className={styles.projectButton}>View Project</Button>
+                    <Button className={styles.codeButton}>View Code</Button>
                 </span>
                 {/* <img className={styles.project} src={softSearch} alt="#" /> */}
                 {/* <img className={styles.project} src={airAware} alt="#" /> */}
