e71567d863d63c2b1cffb5d0145d7a0f98ee79b6
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-18 15:05:11 -0400

    added airaware card

--- src/CONSTANTS/ProjectsConstants.js
+++ src/CONSTANTS/ProjectsConstants.js
@@ -1,8 +1,15 @@
-import { airAware, softSearch } from '../assets'
+import { airAware, softSearch } from "../assets";
 
-export const ssImage = softSearch
+// SoftSearch data
+export const ssImage = softSearch;
 export const ssDescription = `Soft Search is a career search web 
-application specifically created for Software Development.`
-export const ssProjectLink = 'https://s0ft-search.herokuapp.com/'
-export const ssCodeLink = 'https://github.com/ElvisHernandez/SoftSearch'
+application specifically created for Software Development.`;
+export const ssProjectLink = "https://s0ft-search.herokuapp.com/";
+export const ssCodeLink = "https://github.com/ElvisHernandez/SoftSearch";
 
+// AirAware data
+export const aaImage = airAware;
+export const aaDescription = `AirAware is an air pollution tracker created
+so that users can be informed on local and national air quality.`;
+export const aaProjectLink = "https://air-aware.herokuapp.com/";
+export const aaCodeLink = "https://github.com/ElvisHernandez/AirAware";
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,36 +1,39 @@
-import React from 'react';
-import styles from './Projects.module.css'
-import { ProjectsCard } from '../../components'
-import { ssImage, ssDescription, ssProjectLink, ssCodeLink } from '../../CONSTANTS/ProjectsConstants'
-
+import React from "react";
+import styles from "./Projects.module.css";
+import { ProjectsCard } from "../../components";
+import {
+  ssImage,
+  ssDescription,
+  ssProjectLink,
+  ssCodeLink,
+  aaImage,
+  aaDescription,
+  aaProjectLink,
+  aaCodeLink,
+} from "../../CONSTANTS/ProjectsConstants";
 
 const Projects = () => {
-    return (
-        <div className={styles.projectsContainer}>
-            
-            <div className={styles.projectsIntro}><h1>Projects</h1></div>
-            <div className={styles.projects}>
-                <ProjectsCard 
-                    image={ssImage}
-                    description={ssDescription}
-                    projectLink={ssProjectLink}
-                    codeLink={ssCodeLink}
-                />
-                {/* <span className={`${styles.project} ${styles.project1}`}>
-                    <p className={styles.project1Text}>Soft Search is a career search web application specifically created for Software Development.</p>
-                    <Button className={styles.projectButton}>View Project</Button>
-                    <Button className={styles.codeButton}>View Code</Button>
+  return (
+    <div className={styles.projectsContainer}>
+      <div className={styles.projectsIntro}>
+        <h1>Projects</h1>
+      </div>
+      <div className={styles.projects}>
+        <ProjectsCard
+          image={ssImage}
+          description={ssDescription}
+          projectLink={ssProjectLink}
+          codeLink={ssCodeLink}
+        />
+        <ProjectsCard
+          image={aaImage}
+          description={aaDescription}
+          projectLink={aaProjectLink}
+          codeLink={aaCodeLink}
+        />
+      </div>
+    </div>
+  );
+};
 
-                </span>
-                <span className={`${styles.project} ${styles.project2}`}>
-
-                    <Button className={styles.projectButton}>View Project</Button>
-                    <Button className={styles.codeButton}>View Code</Button>
-                </span> */}
-
-            </div>
-        </div>
-    );
-}
- 
 export default Projects;
