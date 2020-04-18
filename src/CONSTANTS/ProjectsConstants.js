14ed76f5b095d558ca581b5a6656ddb4f47f8a77
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-18 16:07:52 -0400

    added technologies used text

--- src/CONSTANTS/ProjectsConstants.js
+++ src/CONSTANTS/ProjectsConstants.js
@@ -6,6 +6,15 @@ const ssDescription = `Soft Search is a career search web
 application specifically created for Software Development.`;
 const ssProjectLink = "https://s0ft-search.herokuapp.com/";
 const ssCodeLink = "https://github.com/ElvisHernandez/SoftSearch";
+const ssTechnologies = [
+  "JavaScript",
+  "HTML",
+  "CSS",
+  "React",
+  "Ruby on Rails",
+  "PostgreSQL",
+  "Bootstrap",
+];
 
 // AirAware data
 const aaImage = airAware;
@@ -13,6 +22,15 @@ const aaDescription = `AirAware is an air pollution tracker created
 so that users can be informed on local and national air quality.`;
 const aaProjectLink = "https://air-aware.herokuapp.com/";
 const aaCodeLink = "https://github.com/ElvisHernandez/AirAware";
+const aaTechnologies = [
+  "JavaScript",
+  "HTML",
+  "CSS",
+  "React",
+  "Node",
+  "Express",
+  "Material UI",
+];
 
 export const projectsJSON = [
   {
@@ -20,11 +38,13 @@ export const projectsJSON = [
     description: ssDescription,
     projectLink: ssProjectLink,
     codeLink: ssCodeLink,
+    technologies: ssTechnologies,
   },
   {
     image: aaImage,
     description: aaDescription,
     projectLink: aaProjectLink,
     codeLink: aaCodeLink,
+    technologies: aaTechnologies,
   },
 ];
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -5,19 +5,29 @@ import { projectsJSON } from "../../CONSTANTS/ProjectsConstants";
 
 const Projects = () => {
   return (
-    <div className={styles.projectsContainer}>
+    <div className={styles.projectsCanvas}>
       <div className={styles.projectsIntro}>
         <h1>Projects</h1>
       </div>
       <div className={styles.projects}>
-        {projectsJSON.map(({ image, description, projectLink, codeLink }) => (
-          <ProjectsCard
-            image={image}
-            description={description}
-            projectLink={projectLink}
-            codeLink={codeLink}
-          />
-        ))}
+        {projectsJSON.map(
+          ({ image, description, projectLink, codeLink, technologies }) => (
+            <div className={styles.projectsContainer}>
+              <h2 className={styles.technologyText}>Technologies Used:</h2>
+              <ul className={styles.technologies}>
+                {technologies.map((tech) => (
+                  <li>{tech}</li>
+                ))}
+              </ul>
+              <ProjectsCard
+                image={image}
+                description={description}
+                projectLink={projectLink}
+                codeLink={codeLink}
+              />
+            </div>
+          )
+        )}
       </div>
     </div>
   );
