be000fc9ed00ec79044570ac71409c8fe5cf0bde
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-25 15:04:02 -0400

    added personal image to about me section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import styles from "./About.module.css";
 
 const About = () => {
-    return (
-        <h1>About</h1>
-    );
-}
- 
+  return (
+    <div className={styles.aboutSection}>
+      <h1 className={styles.aboutMeIntro}>About Me</h1>
+
+      <div className={styles.image}></div>
+    </div>
+  );
+};
+
 export default About;
--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -6,8 +6,6 @@ import { skillsJSON } from "../../CONSTANTS/SkillsConstants";
 const Skills = () => {
   return (
     <div id="skills-section" className={styles.body}>
-      {/* <img style={{borderRadius:'50%', height:'auto', width: 'auto'}} src={elvis} alt=""/> */}
-      {/* <div className={styles.image}></div> */}
       <h1 className={styles.heading}>Skills</h1>
       <div className={styles.skillcards}>
         {skillsJSON.map(({ URL, title, description, skills }) => (
