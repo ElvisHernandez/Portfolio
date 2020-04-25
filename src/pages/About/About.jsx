103bca61e19dec9f3edc3024db3b920f32b970f2
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-25 16:05:51 -0400

    added description and feynman quote

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -8,7 +8,21 @@ const About = () => {
 
       <section className={styles.aboutMe}>
         <div className={styles.image}></div>
-        <div className={styles.description}></div>
+        <div className={styles.description}>
+          {" "}
+          <br />
+          Hi there, my name is <span className={styles.name}>Elvis</span>.{" "}
+          <br /> <br />
+          I'm a full-stack web developer with a passion for learning and problem
+          solving. It was through my education in physics that I discovered my
+          love for computers and the amazingly rich set of problems that they
+          enable one to solve. <br /> <br />
+          <span className={styles.quote}>
+            “Nobody ever figures out what life is all about, and it doesn't
+            matter. Explore the world. Nearly everything is really interesting
+            if you go into it deeply enough.” ― Richard P. Feynman{" "}
+          </span>
+        </div>
       </section>
     </div>
   );
