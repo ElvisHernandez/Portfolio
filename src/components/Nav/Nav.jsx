cb631e835990f137d36960ff8f2fe4e2b4853cde
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-25 14:39:01 -0400

    made it so that links in nav redirect to corresponding html elements

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,112 +1,123 @@
-import React from 'react';
-import { 
-  AppBar, CssBaseline, Divider, Drawer,
-  Hidden, IconButton, List, ListItem, 
-  ListItemIcon, ListItemText, Toolbar } from '@material-ui/core'
-import HttpIcon from '@material-ui/icons/Http';
-import HomeIcon from '@material-ui/icons/Home';
-import CodeIcon from '@material-ui/icons/Code';
-import PermMediaIcon from '@material-ui/icons/PermMedia';
-import FaceIcon from '@material-ui/icons/Face';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import MenuIcon from '@material-ui/icons/Menu';
-import { makeStyles, useTheme, withStyles } from '@material-ui/core/styles';
-import styles from './Nav.module.css'
-import { javascriptPNG } from '../../assets'
+import React from "react";
+import {
+  AppBar,
+  CssBaseline,
+  Divider,
+  Drawer,
+  Hidden,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+} from "@material-ui/core";
+import HttpIcon from "@material-ui/icons/Http";
+import HomeIcon from "@material-ui/icons/Home";
+import CodeIcon from "@material-ui/icons/Code";
+import PermMediaIcon from "@material-ui/icons/PermMedia";
+import FaceIcon from "@material-ui/icons/Face";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import MenuIcon from "@material-ui/icons/Menu";
+import { makeStyles, useTheme, withStyles } from "@material-ui/core/styles";
+import styles from "./Nav.module.css";
+import { javascriptPNG } from "../../assets";
 
 const drawerWidth = "160px";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   drawer: {
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       width: drawerWidth,
       flexShrink: 0,
     },
   },
   appBar: {
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       width: `calc(100% - ${drawerWidth}px)`,
-      height: '50px',
+      height: "50px",
       marginLeft: drawerWidth,
-      background:'#1e1d1d'
+      background: "#1e1d1d",
     },
   },
   menuButton: {
     marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
     },
   },
   // necessary for content to be below app bar
   toolbar: theme.mixins.toolbar,
   drawerPaper: {
     width: drawerWidth,
-    background:'#1e1d1d',
+    background: "#1e1d1d",
     // color:'whitesmoke'
-
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-  }
+  },
 }));
 
 const GlobalCSS = withStyles({
-    "@global" : {
-        ".MuiSvgIcon-root": {
-            color: "#228B22"
-        },
-        ".MuiListItemText-primary": {
-            color: 'whitesmoke',
-            "&:hover": {
-                color:'#00FFFF'
-            }
-        },
-        ".MuiToolbar-gutters": {
-          background: '#1e1d1d',
-          visibility: 'hidden'
-        }, 
-        "#httpIcon": {
-          transform: 'scale(4)',
-          marginTop: '1rem'
-        },
-        "#drawerList": {
-          marginTop:'10rem',
-        },
-        "#externalLinks": {
-          marginTop: '3rem'
-        },
-        ".makeStyles-appBar-3" : {
-          visibility: 'hidden'
-        }, 
-        ".jss3": {
-          visibility: 'hidden'
-        },
-        "@media (max-width: 600px)": {
-          ".makeStyles-appBar-3" : {
-            visibility: 'visible',
-            background: '#1e1d1d'
-          },
-          ".MuiToolbar-gutters": {
-            background: '#1e1d1d',
-            visibility: 'visible'
-          }
-        }
-    }
-})(() => null)
+  "@global": {
+    ".MuiSvgIcon-root": {
+      color: "#228B22",
+    },
+    ".MuiListItemText-primary": {
+      color: "whitesmoke",
+      "&:hover": {
+        color: "#00FFFF",
+      },
+    },
+    ".MuiToolbar-gutters": {
+      background: "#1e1d1d",
+      visibility: "hidden",
+    },
+    "#httpIcon": {
+      transform: "scale(4)",
+      marginTop: "1rem",
+    },
+    "#drawerList": {
+      marginTop: "10rem",
+    },
+    "#externalLinks": {
+      marginTop: "3rem",
+    },
+    ".makeStyles-appBar-3": {
+      visibility: "hidden",
+    },
+    ".jss3": {
+      visibility: "hidden",
+    },
+    "@media (max-width: 600px)": {
+      ".makeStyles-appBar-3": {
+        visibility: "visible",
+        background: "#1e1d1d",
+      },
+      ".MuiToolbar-gutters": {
+        background: "#1e1d1d",
+        visibility: "visible",
+      },
+    },
+  },
+})(() => null);
 
 const iconHash = {
-  0: [<HomeIcon />,'#particles-js'],
-  1: [<CodeIcon />, '#'],
-  2: [<PermMediaIcon />, '#'],
-  3: [<FaceIcon />, '#'],
-  4: [<LinkedInIcon />, "https://www.linkedin.com/in/elvis-hernandez-9669a618a/"],
-  5: [<GitHubIcon />, 'https://github.com/ElvisHernandez']
-}
+  0: [<HomeIcon />, "#particles-js"],
+  1: [<CodeIcon />, "#skills-section"],
+  2: [<PermMediaIcon />, "#projects-section"],
+  3: [<FaceIcon />, "#"],
+  4: [
+    <LinkedInIcon />,
+    "https://www.linkedin.com/in/elvis-hernandez-9669a618a/",
+  ],
+  5: [<GitHubIcon />, "https://github.com/ElvisHernandez"],
+};
 
 function ResponsiveDrawer(props) {
   const { container } = props;
@@ -120,14 +131,16 @@ function ResponsiveDrawer(props) {
 
   const drawer = (
     <div className={styles.drawer}>
-      <HttpIcon id="httpIcon"/>
+      <HttpIcon id="httpIcon" />
       {/* <div className={classes.toolbar} />   uncomment this line if you have a problem with the navbar */}
       <Divider />
       <List id="drawerList">
-        {['Home', 'Skills', 'Projects', 'About'].map((text, index) => (
-          <a className={styles.links} href={iconHash[index][1]} target='_blank'>
+        {["Home", "Skills", "Projects", "About"].map((text, index) => (
+          <a className={styles.links} href={iconHash[index][1]}>
             <ListItem button key={text}>
-              <ListItemIcon className={styles.icons}>{iconHash[index][0]}</ListItemIcon>
+              <ListItemIcon className={styles.icons}>
+                {iconHash[index][0]}
+              </ListItemIcon>
               <ListItemText className={styles.text} primary={text} />
             </ListItem>
           </a>
@@ -135,16 +148,20 @@ function ResponsiveDrawer(props) {
       </List>
       <Divider />
       <List id="externalLinks">
-        {['LinkedIn', 'Github'].map((text, index) => (
-          <a className={styles.links} href={iconHash[index+4][1]} target='_blank'>
+        {["LinkedIn", "Github"].map((text, index) => (
+          <a
+            className={styles.links}
+            href={iconHash[index + 4][1]}
+            target="_blank"
+          >
             <ListItem button key={text}>
-              <ListItemIcon>{iconHash[index+4][0]}</ListItemIcon>
+              <ListItemIcon>{iconHash[index + 4][0]}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           </a>
         ))}
       </List>
-      <img className={styles.javascript} src={javascriptPNG} alt=""/>
+      <img className={styles.javascript} src={javascriptPNG} alt="" />
     </div>
   );
 
@@ -152,9 +169,9 @@ function ResponsiveDrawer(props) {
     <div className={classes.root}>
       <GlobalCSS />
       <CssBaseline />
-      
+
       <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar >
+        <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -170,12 +187,10 @@ function ResponsiveDrawer(props) {
         </Toolbar>
       </AppBar>
 
-
-
       <nav className={classes.drawer} aria-label="mailbox folders">
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Hidden smUp implementation="css">
-          <Drawer 
+          <Drawer
             container={container}
             variant="temporary"
             // anchor={theme.direction === 'rtl' ? 'right' : 'left'}
